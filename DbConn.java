e716ed423906c329871a3d5e4fdcffbb3e79ac33
Allan Gitobu <allan.mugambi@cgu.edu>
2014-01-22 07:00:45 -0800

    Create DbConn.java

---
+++ DbConn.java
@@ -0,0 +1,12 @@
+package Collection;
+
+public class DbConn {
+	// JDBC driver name and database URL
+	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost/Books";
+
+	   //  Database credentials
+	   static final String USER = "root";
+	   static final String PASS = "ist303@cgu";
+
+}
